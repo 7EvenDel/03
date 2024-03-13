6269ec54a2bd8da9953b600c5390ca21c240e629
7EvenDel <116507218+7EvenDel@users.noreply.github.com>
2024-03-13 10:45:39 +0200

    Add files via upload

--- ex06.py
+++ ex06.py
@@ -1,56 +1,64 @@
+def count_negative(a, b, c):
+  negative_count = 0
+  if a < 0:
+    negative_count += 1
+  if b < 0:
+    negative_count += 1
+  if c < 0:
+    negative_count += 1
+
+  return negative_count
+
+def count_positive(a, b, c):
+  positive_count = 0
+  if a > 0:
+    positive_count += 1
+  if b > 0:
+    positive_count += 1
+  if c > 0:
+    positive_count += 1
+
+  return positive_count
+
+def count_integer(a, b, c):
+  integer_count = 0
+  if a.is_integer():
+    integer_count += 1
+  if b.is_integer():
+    integer_count += 1
+  if c.is_integer():
+    integer_count += 1
+
+  return integer_count
+
+def count_divisible(a, b, c, k):
+  print("Число k є дільником:")
+  if a % k == 0:
+    print("a = ", a)
+  if b % k == 0:
+    print("b = ", b)
+  if c % k == 0:
+    print("c = ", c)
+
+  print("Жодного з чисел a, b, c.")
+
+
 a = float(input("Введіть число a: "))
 b = float(input("Введіть число b: "))
 c = float(input("Введіть число c: "))
 
-#1)
-negative_count = 0
-if a < 0:
-  negative_count += 1
-if b < 0:
-  negative_count += 1
-if c < 0:
-  negative_count += 1
 
+negative_count = count_negative(a, b, c)
 print("Кількість негативних чисел: ", negative_count)
 
-#2)
-positive_count = 0
-if a > 0:
-  positive_count += 1
-if b > 0:
-  positive_count += 1
-if c > 0:
-  positive_count += 1
 
+positive_count = count_positive(a, b, c)
 print("Кількість додатних чисел: ", positive_count)
 
-#3)
-is_a_integer = a.is_integer()
-is_b_integer = b.is_integer()
-is_c_integer = c.is_integer()
 
-integer_count = 0
-if is_a_integer:
-  integer_count += 1
-if is_b_integer:
-  integer_count += 1
-if is_c_integer:
-  integer_count += 1
-
+integer_count = count_integer(a, b, c)
 print("Кількість цілих чисел: ", integer_count)
 
-#4)
+
 k = int(input("Введіть число k: "))
-
-is_a_divisible = a % k == 0
-is_b_divisible = b % k == 0
-is_c_divisible = c % k == 0
-print("Число k є дільником:")
-if is_a_divisible:
-  print("a = ", a)
-if is_b_divisible:
-  print("b = ", b)
-if is_c_divisible:
-  print("c = ", c)
-
-print("Жодного з чисел a, b, c.")
+count_divisible(a, b, c, k)
